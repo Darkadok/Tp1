20cad6213e1ca5a2ee23e2bc13d36fdbd7cf4e9e
Merlin Loison <darkadok@gmail.com>
2015-03-19 16:30:57 +0100

    del fichier tmp

--- test/test_app.bat.BASE.5808.bat
+++
@@ -1,6 +0,0 @@
-### application.bat - 2015 - Loison ###
-version = "2"
-build_hour = "11H00"
-
-echo "Build ${build_hour}"
-echo "version ${version}"
--- test/test_app.bat.REMOTE.5808.bat
+++
@@ -1,6 +0,0 @@
-### application.bat - 2015 - Loison ###
-version = "1.2.0-b"
-build_hour = "11H00"
-
-echo "Build ${build_hour}"
-echo "version ${version}"
